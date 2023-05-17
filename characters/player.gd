998a6a5c0d8454cb8abfc7dc890a200c47391e24
IceCobe <jaalaba@gmail.com>
2023-05-16 21:48:12 -0400

    player now has controlled walking

--- characters/player.gd
+++ characters/player.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var start_direction : Vector2 = Vector2(0,1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _ready():
+	update_animation_parameters(start_direction)
 
 func _physics_process(_delta):
 	# get input direction
@@ -9,10 +16,29 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 
-	print(input_direction)
+	update_animation_parameters(input_direction)
 
 	# update velocity
 	velocity = input_direction * move_speed
-
+	
+	# Flip the sprite horizontally
+	if input_direction.x < 0:
+		$Sprite2D.scale.x = -1
+	elif input_direction.x > 0:
+		$Sprite2D.scale.x = 1
+		
 	# move and slide function uses velocity of hcaracter bodyto move character on map
 	move_and_slide()
+	pick_new_state()
+
+func update_animation_parameters(move_input : Vector2):
+	# Don't change animation parameters if there is no move input
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/walking/blend_position", move_input)
+		animation_tree.set("parameters/idle/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walking")
+	else:
+		state_machine.travel("idle")
