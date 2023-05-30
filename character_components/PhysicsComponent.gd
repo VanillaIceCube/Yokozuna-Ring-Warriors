99dd2fb2d83563d5ab4c6edd5db25b08e4f7a1bb
IceCobe <jaalaba@gmail.com>
2023-05-29 20:04:00 -0400

    physics_component comments added

--- character_components/PhysicsComponent.gd
+++ character_components/PhysicsComponent.gd
@@ -1,13 +1,13 @@
 extends Node
 
-var parent_node: Node = null
+var parent_node: Node = null # Initializing parent_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	parent_node = get_parent()
+	parent_node = get_parent() # Setting the parent_node
 
 	if parent_node is RigidBody2D:
-		parent_node.gravity_scale = 0.0
-		parent_node.lock_rotation = true
+		parent_node.gravity_scale = 0.0  # Not affected by gravity
+		parent_node.lock_rotation = true # Rotation locked
 	else:
-		assert(false, "PhysicsComponent's parent isn't a RigidBody2D")
+		assert(false, "PhysicsComponent's parent isn't a RigidBody2D") # Basic error proofing
