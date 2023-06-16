4ff92d95eec838a714ddc0b77d195ae1cc987344
IceCobe <jaalaba@gmail.com>
2023-06-15 21:01:31 -0400

    based physics class created

---
+++ character_scripts/RigidBody2DBase.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+class_name RigidBody2DBase
+
+# Initializing movement related variables
+var target_position: Vector2 = Vector2(0, 0)
+var target_direction: Vector2 = Vector2.ZERO
+var distance_to_target: float = 0.0
+var target_is_enemy: bool = false
+var dead: bool = false
+var tar_group: String = "Chest"
+
+var verbosity: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
