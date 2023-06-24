7efcdfa5392c23e01519162e020127ba7b131a8f
IceCobe <jaalaba@gmail.com>
2023-06-24 11:22:24 -0400

    Revert "Empty arena & auto connecting signals added"
    This reverts commit af00f93c9dd2ba9b95f888b00a433185bd47e192.

--- game_objects/BaseRoom.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready():
-	add_child(preload("res://arenas/Arena_01.tscn").instantiate())
-	add_child(preload("res://characters/Yokotuna.tscn").instantiate())
-	add_child(preload("res://game_objects/TunaSpawner.tscn").instantiate())
